9e1b7e783289a84cbd596d24181353b871360712
juhay-git <juha.yli-hemminki@seamk.fi>
2023-03-27 11:25:48 +0300

    eka kommitti

---
+++ flask_app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+lampotilat = [
+    {'x':1, 'y':14},
+    {'x':2, 'y':10},
+    {'x':3, 'y':15}
+]
+
+paivat = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai']
+
+@app.route('/api', methods=['GET'])
+def index():
+    return render_template("mittaukset.html", taulukko=lampotilat, paivat=paivat)
+
+@app.route('/lisaa', methods=['POST'])
+def lisaa():
+    uusimittaus = request.get_json(force=True)
+    lampotilat.append(uusimittaus)
+    return(json.dumps(uusimittaus))
+
+if __name__ == "__main__":
+    app.run(debug=True)
